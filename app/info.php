f98d3b4af95cab6eb4905060602624f07ebfdcdf
Mostafa20051 <mostafashawahne68@gmail.com>
2026-06-11 01:12:26 +0300

    Use environment variables for database configuration

--- app/info.php
+++ app/info.php
@@ -1,7 +1,11 @@
 <?php
 
-$conn = new mysqli("db", "appuser", "apppass", "dictionary_app");
-
+$conn = new mysqli(
+    getenv("DB_HOST"),
+    getenv("DB_USER"),
+    getenv("DB_PASSWORD"),
+    getenv("DB_NAME")
+);
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
